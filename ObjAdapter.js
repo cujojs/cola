48c0875a98d735c720a86371e4eeca364546c76f
John Hann <john@e-numera.com>
2012-02-21 16:18:05 -0500

    addsyncTo() and remove Watchable

--- ObjAdapter.js
+++ ObjAdapter.js
@@ -1,12 +1,8 @@
 /** MIT License (c) copyright B Cavalier & J Hann */
 
 (function (define) {
-define(function (require) {
-	"use strict";
-
-	var makeWatchable;
-
-	makeWatchable = require('./Watchable');
+define(function () {
+"use strict";
 
 	/**
 	 * @constructor
@@ -15,7 +11,8 @@ define(function (require) {
 	 */
 	function ObjAdapter(obj) {
 
-		this._watchable = makeWatchable(obj);
+		this._obj = obj;
+		this._listeners = {};
 
 		// set blank bindings
 		this.setBindings({});
@@ -24,17 +21,30 @@ define(function (require) {
 	ObjAdapter.prototype = {
 
 		watch: function (name, callback) {
-			return this._watchable.watch(name, callback);
+			return listen(this._listeners, name, callback);
 		},
 
 		watchAll: function (callback) {
-			return this._watchable.watch('*', callback);
+			return listen(this._listeners, '*', callback);
 		},
 
+//		get: function (name) {
+//			return this._obj[name];
+//		},
+
 		set: function (name, value) {
-			// note: this has an intended side-effect: watchers will
-			// be notified.
-			this._watchable.set(name, value);
+			var obj = this._obj;
+			if (obj[name] != value) {
+				obj[name] = value;
+				notify(this._listeners, value, name);
+				notify(this._listeners, value, '*', name); // also notify any wildcard listeners
+			}
+		},
+
+		syncTo: function (adapter) {
+			for (var p in this._obj) {
+				adapter.set(this._obj[p]);
+			}
 		},
 
 		/**
@@ -63,9 +73,62 @@ define(function (require) {
 
 	return ObjAdapter;
 
+	/**
+	 * Registers a listener and returns a function to unlisten.
+	 * Internal implementation of watch/unwatch.
+	 * @private
+	 * @param name {String}
+	 * @param callback {Function}
+	 */
+	function listen (listeners, name, callback) {
+		var list, head;
+
+		list = listeners[name];
+		head = { callback: callback, prev: list };
+		listeners[name] = head;
+
+		// return unwatch function
+		return function () {
+			walkList(listeners[name], function (item) {
+				if (item == head) {
+					listeners[name] = head.prev;
+					return walkList.stopSignal;
+				}
+			});
+		};
+	}
+
+	/**
+	 * Calls all listener functions with the details of a modified
+	 * property.
+	 * @private
+	 * @param value
+	 * @param key
+	 */
+	function notify (listeners, value, key, name) {
+		if (arguments.length < 3) name = key;
+		walkList(listeners[key], function (item) {
+			item.callback(name, value);
+		});
+	}
+
+	/**
+	 * Walks a linked list
+	 * @private
+	 * @param list
+	 * @param callback {Function} function (itemInList, callback) {}
+	 */
+	function walkList (list, callback) {
+		var item = list;
+		while (item && callback(item) !== walkList.stopSignal) {
+			item = item.prev;
+		}
+	}
+	walkList.stopSignal = {};
+
 });
 }(
 	typeof define == 'function'
 		? define
-		: function (factory) { module.exports = factory(require); }
+		: function (factory) { module.exports = factory(); }
 ));
