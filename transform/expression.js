b443b293309065183c6814cf4f5d0ed6a6e792aa
Brian Cavalier <brian@hovercraftstudios.com>
2012-03-14 09:24:08 -0400

    Removed unused item param, and update docs

--- transform/expression.js
+++ transform/expression.js
@@ -5,11 +5,12 @@ define(function () {
 "use strict";
 
 	/**
-	 * Creates a function that can be used to evaluate arbitrary expressions
-	 * on a data item.
-	 * @param options {Object}
-	 * @returns {Function} function (value, item) {}
-	 * @description
+	 * Transforms the supplied property value using the supplied expression
+	 * @param value anything - the value to transform
+	 * @param propName {String} the name of the property being transformed
+	 * @param expression {String} the expression to execute.  It will have
+	 * access to variables named "value" and "propName".
+	 * @returns transformed value
 	 */
 	return function expressionTransform (value, propName, expression) {
 		try {
@@ -25,9 +26,9 @@ define(function () {
 	typeof define == 'function'
 		? define
 		: function (factory) { module.exports = factory(); },
-	function (value, propName, item) {
+	function (value, propName) {
 		var window, document;
-		// the only variables in scope are value, item, and any globals
+		// the only variables in scope are value and propName, and any globals
 		// not listed in the var statement above. we have to cast to string
 		// because of "oddities" between `eval` and `this`.
 		return eval('' + this);
