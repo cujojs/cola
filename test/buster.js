a54838198cce839431ea567e35980334452717b6
Brian Cavalier <brian@hovercraftstudios.com>
2012-03-16 13:51:54 -0400

    Unit tests for leftOuterJoin strategy

--- test/buster.js
+++ test/buster.js
@@ -1,7 +1,8 @@
 exports['shared tests'] = {
 	tests: ['ArrayAdapter.js', 'ResultSetAdapter.js', 'ResultAdapter.js', 'QueryAdapter.js',
 		'transformCollection.js', 'SortedMap.js',
-		'mediator/*.js', 'transform/*.js', 'relational/*.js', 'projection/*.js']
+		'mediator/*.js', 'transform/*.js', 'relational/*.js', 'relational/strategy/*.js',
+		'projection/*.js']
 };
 
 exports['node tests'] = {
