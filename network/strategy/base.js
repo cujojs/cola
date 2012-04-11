544b5a8f0cf544ea7d84f51db9b1b855c4e82cc3
John Hann <john@e-numera.com>
2012-04-11 16:27:32 -0400

    more meaningful comment

--- network/strategy/base.js
+++ network/strategy/base.js
@@ -1,18 +1,13 @@
 (function (define) {
 define(function () {
 
+	/**
+	 * Creates a base strategy function.
+	 * @param options {Object} not currently used
+	 * @return {Function} a strategy function
+	 */
 	return function (options) {
 
-		/**
-		 * Base network event strategy function.
-		 * @type strategyFunction
-		 * @private
-		 * @param source {Object} the adapter that sourced the event
-		 * @param dest {Object} the adapter receiving the event
-		 * @param data {Object} any data associated with the event
-		 * @param type {String} the type of event
-		 * @param api {Object} helpful functions for strategies
-		 */
 		return function baseStrategy (source, dest, data, type, api) {
 			if (type in dest && source != dest) {
 				if (typeof dest[type] != 'function') {
