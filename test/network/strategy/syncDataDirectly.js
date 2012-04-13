da7d13f58b000d36e2738b6aa83d5fcf55671718
John Hann <john@e-numera.com>
2012-04-13 10:25:11 -0400

    initial stab at unit tests. gonna commit this and merge into dev

--- test/network/strategy/syncDataDirectly.js
+++ test/network/strategy/syncDataDirectly.js
@@ -7,8 +7,22 @@ refute = buster.refute;
 
 var syncDataDirectly = require('cola/network/strategy/syncDataDirectly');
 
+var fakeApi = function (phase) {
+	return {
+		phase: phase,
+		isBefore: function () { return this.phase == 'before'; }
+	};
+};
+
 buster.testCase('cola/network/strategy/syncDataDirectly', {
 
+	'should return a function': function () {
+		assert.isFunction(syncDataDirectly());
+	},
+	'should always return false for a "sync"': function () {
+		var strategy = syncDataDirectly();
+		assert.equals(false, strategy({}, {}, {}, 'sync', fakeApi('before')));
+	},
 	'// should do something': function () {
 		assert(false);
 	}
